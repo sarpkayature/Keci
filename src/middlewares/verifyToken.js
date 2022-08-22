cb35f0437b3600e07823ea772ea70fd8caae01f9
sarpkayature <sarpkayature@gmail.com>
2022-08-22 23:08:02 +0300

    fix: verify token middleware

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -3,19 +3,24 @@ const JWT = jsonwebtoken;
 
 const verifyToken = async (req, res, next) => {
   const { authorization } = req.headers;
-  const token = authorization.split(' ')[1];
+
+  const token = authorization?.split(' ')[1];
   try {
-    const jt = await JWT.verify(token, process.env.JWT_SECRET);
-    if (jt) {
-      next();
-    } else {
-      res.status(400).send({
-        status: 400,
-        message: 'Invalid Token',
-      });
-    }
+    await JWT.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).send({
+          status: 401,
+          message: 'Invalid Token',
+        });
+      } else {
+        next();
+      }
+    });
   } catch (error) {
-    res.status(401).send('Unauthorized');
+    res.status(401).send({
+      status: 401,
+      message: 'Invalid Token',
+    });
   }
 };
 
